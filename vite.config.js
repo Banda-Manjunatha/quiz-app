770c89b9c5420deea7b8623ecad3065800834f9a
Manjunatha Banda <bandamanjunatha2002@gmail.com>
2025-02-18 23:36:56 +0530

    2

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { remixVitePlugin } from "@remix-run/dev";
+
+// Import `remixVitePlugin` from CommonJS
+import remixDev from "@remix-run/dev";
+const remixVitePlugin = remixDev.remixVitePlugin;
 
 export default defineConfig({
     plugins: [react(), remixVitePlugin()],
