d0f2716cc4404ae5a3c06e5f19606c4f4b961690
Manjunatha Banda <bandamanjunatha2002@gmail.com>
2025-02-19 00:06:24 +0530

    7

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Import default from @remix-run/dev
-import remixDev from "@remix-run/dev";
-
 export default defineConfig({
-    plugins: [react(), remixDev.remixVitePlugin()],
+    plugins: [
+        react(),
+        async () => {
+            const remixDev = await import("@remix-run/dev");
+            const remixVitePlugin = remixDev.vitePlugin; // Correct property name: vitePlugin
+            return remixVitePlugin();
+        },
+    ],
 });
