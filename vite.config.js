e8120dbc65d796dbf539776350ccd51b7cdd8332
Manjunatha Banda <bandamanjunatha2002@gmail.com>
2025-02-18 23:26:26 +0530

    asd

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { remixVitePlugin } from "@remix-run/dev"; // Import the plugin
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        remixVitePlugin(), // Add the Remix plugin here
+    ],
 });
